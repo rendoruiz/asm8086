5e59e59b886a6fa0c05bfe54a64e0ee75870c9de
Rendo Ruiz <46543419+rendoruiz@users.noreply.github.com>
2022-01-31 14:45:29 -0700

    gpl notice

--- snake/assembly snake v0.4.asm
+++ snake/assembly snake v0.4.asm
@@ -1,3 +1,20 @@
+; Recon Game Library is a library that implements snake and tictactoe game 
+; in 8086 assembly.
+; Copyright (C) 2022 Rendo Ruiz
+
+; This program is free software: you can redistribute it and/or modify
+; it under the terms of the GNU General Public License as published by
+; the Free Software Foundation, either version 3 of the License, or
+; (at your option) any later version.
+
+; This program is distributed in the hope that it will be useful,
+; but WITHOUT ANY WARRANTY; without even the implied warranty of
+; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+; GNU General Public License for more details.
+
+; You should have received a copy of the GNU General Public License
+; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 ; Snake (Alpha v0.4): refactor code
  
 data segment
